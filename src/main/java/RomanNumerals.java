5c8aa6f94b17ab42654362b81a5328759c2dcf5b
Simon <simonrosenberg1@gmail.com>
2021-09-07 09:48:33 +0100

    Test: Add tests from 1 to 50

--- src/main/java/RomanNumerals.java
+++ src/main/java/RomanNumerals.java
@@ -1,2 +1,27 @@
 public class RomanNumerals {
+    public String convert(int amount) {
+
+        String romanNumeral = "";
+
+        while (amount > 0) {
+            if (amount >= 50) {
+                romanNumeral += "L";
+                amount -= 50;
+                continue;
+            }
+            if (amount >= 10) {
+                romanNumeral += "X";
+                amount -= 10;
+                continue;
+            }
+            if (amount >= 5) {
+                romanNumeral += "V";
+                amount -= 5;
+                continue;
+            }
+            romanNumeral += "I";
+            amount -= 1;
+        }
+        return romanNumeral;
+    }
 }
--- src/test/java/RomanNumeralsTest.java
+++ src/test/java/RomanNumeralsTest.java
@@ -1,2 +1,54 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class RomanNumeralsTest {
+
+    @Test
+    public void shouldReturnIWhenGiven1() {
+        RomanNumerals romanNumerals = new RomanNumerals();
+        assertEquals("I", romanNumerals.convert(1));
+    }
+
+    @Test
+    public void shouldReturnIIWhenGiven2() {
+        RomanNumerals romanNumerals = new RomanNumerals();
+        assertEquals("II", romanNumerals.convert(2));
+    }
+
+    @Test
+    public void shouldReturnIIIWhenGiven3() {
+        RomanNumerals romanNumerals = new RomanNumerals();
+        assertEquals("III", romanNumerals.convert(3));
+    }
+
+    @Test
+    public void shouldReturnVWhenGiven5() {
+        RomanNumerals romanNumerals = new RomanNumerals();
+        assertEquals("V", romanNumerals.convert(5));
+    }
+
+    @Test
+    public void shouldReturnVIWhenGiven6() {
+        RomanNumerals romanNumerals = new RomanNumerals();
+        assertEquals("VI", romanNumerals.convert(6));
+    }
+
+    @Test
+    public void shouldReturnXWhenGiven10() {
+        RomanNumerals romanNumerals = new RomanNumerals();
+        assertEquals("X", romanNumerals.convert(10));
+    }
+
+    @Test
+    public void shouldReturnXIWhenGiven11() {
+        RomanNumerals romanNumerals = new RomanNumerals();
+        assertEquals("XI", romanNumerals.convert(11));
+    }
+
+    @Test
+    public void shouldReturnLWhenGiven50() {
+        RomanNumerals romanNumerals = new RomanNumerals();
+        assertEquals("L", romanNumerals.convert(50));
+    }
 }
